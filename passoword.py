5d6459f3e5ece7348b7ae999212539d8d1e47e61
yagmur-kolanc <yagmur.kolanci@mf.karaelmas.edu.tr>
2021-04-10 12:14:06 +0300

    Create passoword.py

---
+++ passoword.py
@@ -0,0 +1,9 @@
+parola=str(input("enter parola"))
+len(parola)
+print(len(parola))
+for j in parola:
+     if j == ('@' 'x'):
+         print("gecersiz parola")
+     else:
+         print(parola)
+     break
